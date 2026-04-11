25d5ce969c8bec53c7ff6b2279c69e591165a887
ivan-pelly <ivan.pelly@gmail.com>
2026-04-10 17:53:16 -0700

    latest

---
+++ db/Objects/tables/student.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `student` (
+  `id_student` char(36) NOT NULL,
+  `id_program` char(36) DEFAULT NULL,
+  `identifier` varchar(50) DEFAULT NULL,
+  `program_year` int DEFAULT NULL,
+  `enrollment_date` date DEFAULT NULL,
+  `next_iep_date` date DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id_student`),
+  KEY `student_ibfk_1` (`id_program`),
+  CONSTRAINT `student_ibfk_1` FOREIGN KEY (`id_program`) REFERENCES `program` (`id_program`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
