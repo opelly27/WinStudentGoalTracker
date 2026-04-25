1d684827a3a95659bd9ef943153089b74d66e097
Armin Abaye <aabaye3@gatech.edu>
2026-04-24 21:24:03 -0400

    next round of ux updates

--- ui/winstudentgoaltracker/src/app/desktop/components/modal-shell/modal-shell.ts
+++ ui/winstudentgoaltracker/src/app/desktop/components/modal-shell/modal-shell.ts
@@ -1,13 +1,35 @@
-import { Component, input, output } from '@angular/core';
+import { Component, ElementRef, HostListener, inject, input, OnDestroy, OnInit, output } from '@angular/core';
 
 @Component({
     selector: 'app-modal-shell',
     templateUrl: './modal-shell.html',
     styleUrl: './modal-shell.scss',
 })
-export class ModalShell {
+export class ModalShell implements OnInit, OnDestroy {
     readonly title = input.required<string>();
     readonly closed = output<void>();
+
+    private readonly el = inject(ElementRef);
+    private previousFocus: HTMLElement | null = null;
+
+    ngOnInit() {
+        this.previousFocus = document.activeElement as HTMLElement;
+        requestAnimationFrame(() => {
+            const focusable = this.el.nativeElement.querySelector<HTMLElement>(
+                'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+            );
+            focusable?.focus();
+        });
+    }
+
+    ngOnDestroy() {
+        this.previousFocus?.focus();
+    }
+
+    @HostListener('keydown.escape')
+    onEscape() {
+        this.closed.emit();
+    }
 
     onOverlayClick() {
         this.closed.emit();
